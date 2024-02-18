c558997e51c7f05037daab730c6e4326022a7b16
bayuuu02 <panciwarteg100@gmail.com>
2024-02-18 23:37:43 +0700

    bayu cv

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".kolagen-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
